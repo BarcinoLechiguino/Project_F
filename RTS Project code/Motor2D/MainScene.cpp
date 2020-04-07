7f89434425205abc5a8fb7a78043af0fca6788e4
Fran Guerrero <franguerreroo98@gmail.com>
2020-04-07 19:41:45 +0200

    .

--- RTS Project code/Motor2D/MainScene.cpp
+++ RTS Project code/Motor2D/MainScene.cpp
@@ -66,22 +66,26 @@ void MainScene::LoadGuiElements()
 
 	window_center = iPoint(win_width * 0.5f, win_height * 0.5f);
 
-	SDL_Rect button_size				= { 0, 0, 230, 18 };
-	SDL_Rect transition_button_idle		= { 0, 202, 230, 18 };
-	SDL_Rect transition_button_hover	= { 0, 224, 230, 18 };
-	SDL_Rect transition_button_clicked	= { 0, 158, 230, 18 };
+	SDL_Rect button_size				= { 0, 0, 348, 51 };
+	SDL_Rect transition_button_idle		= { 4, 16, 348, 51 };
+	SDL_Rect transition_button_hover	= { 395, 16, 348, 51 };
+	SDL_Rect transition_button_clicked	= { 784, 16, 348, 51 };
 	
-	iPoint center_button = iPoint(window_center.x - button_size.w * 0.5, window_center.y - button_size.h * 0.5f);
+	//iPoint center_button = iPoint(window_center.x - button_size.w * 0.5, window_center.y - button_size.h * 0.5f);
 
-	transition_button = (UI_Button*)App->gui->CreateButton(UI_Element::BUTTON, center_button.x, center_button.y, true, true, false, this, nullptr
+	transition_button = (UI_Button*)App->gui->CreateButton(UI_Element::BUTTON,0, 0, true, true, false, this, nullptr
 													, &transition_button_idle, &transition_button_hover, &transition_button_clicked);
 
-	SDL_Rect text_rect = { 0, 0, 40, 9 };
-	_TTF_Font* font = App->font->Load("fonts/Minecraftia-Regular.ttf", 12);
-	std::string button_string = "To Gameplay Scene";
+	//SDL_Rect text_rect = { 0, 0, 40, 9 };
+	//_TTF_Font* font = App->font->Load("fonts/Minecraftia-Regular.ttf", 12);
+	//std::string button_string = "To Gameplay Scene";
 
-	button_text = (UI_Text*)App->gui->CreateText(UI_Element::TEXT, center_button.x + text_rect.w, center_button.y + 3, text_rect, font
-													, SDL_Color{ 255, 255, 255, 255 }, true, false, false, nullptr, transition_button, &button_string);
+	//button_text = (UI_Text*)App->gui->CreateText(UI_Element::TEXT, center_button.x + text_rect.w, center_button.y + 3, text_rect, font
+	//												, SDL_Color{ 255, 255, 255, 255 }, true, false, false, nullptr, transition_button, &button_string);
+
+
+
+
 }
 
 void MainScene::OnEventCall(UI* element, UI_Event ui_event)
