2eb768a5ff6100b1bfc276507dec5b77a3c65fcb
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2020-05-11 20:02:43 +0200

    Display HUD menu working

--- RTS Project code/Motor2D/GameplayScene.cpp
+++ RTS Project code/Motor2D/GameplayScene.cpp
@@ -1025,6 +1025,16 @@ void GameplayScene::LoadGuiElements()
 		, &HUD_missions_tab_idle, &HUD_missions_tab_hover, &HUD_missions_tab_clicked);
 
 	
+	//Tab close 
+	SDL_Rect HUD_missions_tab_close_size = { 0, 0, 30, 81 };
+	SDL_Rect HUD_missions_tab_close_idle = { 780, 117, 30, 81 };
+	SDL_Rect HUD_missions_tab_close_hover = { 813, 117, 30, 81 };
+	SDL_Rect HUD_missions_tab_close_clicked = { 846, 117, 30, 81 };
+
+	HUD_missions_tab_close = (UI_Button*)App->gui_manager->CreateButton(UI_ELEMENT::BUTTON, 883, 389, false, true, false, this, nullptr
+		, &HUD_missions_tab_close_idle, &HUD_missions_tab_close_hover, &HUD_missions_tab_close_clicked);
+
+
 	//Back
 	SDL_Rect HUD_missions_back_size = { 25, 400, 390, 226 };
 
@@ -1308,11 +1318,22 @@ void GameplayScene::OnEventCall(UI* element, UI_EVENT ui_event)
 		//	HUD_missions_tab->SetScreenPos(iPoint(N_Lerp(1252, 10, 0.1, false), 503));
 		//}
 
-		HUD_missions_tab->SetScreenPos(iPoint(883, 389));
-		HUD_missions_tab->SetHitbox({ 883,389,30, 81 });
+		App->gui_manager->SetElementsVisibility(HUD_missions_tab, false);
+		App->gui_manager->SetElementsVisibility(HUD_missions_tab_close, true);
 
 		HUD_missions_background->SetScreenPos(iPoint(902, 359));
 		HUD_missions_background->SetHitbox({ 902,359,390, 226 });
+	}
+
+	if (element == HUD_missions_tab_close && ui_event == UI_EVENT::UNCLICKED)
+	{
+		App->audio->PlayFx(App->gui_manager->standard_fx, 0);
+
+		HUD_missions_background->SetScreenPos(iPoint(1276, 359));
+		HUD_missions_background->SetHitbox({ 1276,359,390, 226 });
+
+		App->gui_manager->SetElementsVisibility(HUD_missions_tab, true);
+		App->gui_manager->SetElementsVisibility(HUD_missions_tab_close, false);
 	}
 }
 
