a8148d5d934b7ed53dd974e3a61bf9dbbff3faf8
paupedra <paupe200@gmail.com>
2020-05-18 20:01:30 +0200

    Tutorial finished and completable

--- Source/Motor2D/GameplayScene.cpp
+++ Source/Motor2D/GameplayScene.cpp
@@ -332,12 +332,14 @@ void GameplayScene::HandleTutorial()
 
 		break;
 	case TutorialState::GATHER_MORE_RESOURCES:
+		if (App->entity_manager->resource_electricity >= 5) //Tutorial 5
+		{
+			tutorial.tutorial_state = TutorialState::RECRUIT_INFANTRY;
+			App->dialog_manager->StartDialog(5);
+		}
 
 		break;
 	case TutorialState::RECRUIT_INFANTRY:
-
-		break;
-	case TutorialState::ATTACK_ENEMY:
 
 		break;
 	}
@@ -456,6 +458,12 @@ void GameplayScene::SpawnAllyUnit(ENTITY_TYPE type)
 				barrack = (Barracks*)App->player->building_selected;
 				App->audio->PlayFx(App->gui_manager->recruit_unit_button_clicked_fx, 0);
 				barrack->creation_queue.push_back(ENTITY_TYPE::INFANTRY);
+
+				if (tutorial.tutorial_state == TutorialState::RECRUIT_INFANTRY)//Tutorial 6
+				{
+					tutorial.tutorial_state = TutorialState::NOT_ACTIVE;
+					App->dialog_manager->StartDialog(6);
+				}
 			}
 			break;
 
